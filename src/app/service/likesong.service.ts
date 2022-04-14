cb688e374faf853e67d3250103103c08b5117ed1
TranAnhHao23 <Trananhhaonuce@gmail.com>
2022-04-15 00:04:20 +0700

    đổi tên

--- src/app/service/likesong.service.ts
+++ src/app/service/likesong.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {LikeSong} from '../model/LikeSong';
 import {HttpService} from './http.service';
 
 const API_URL = `${environment.apiUrl}`;
